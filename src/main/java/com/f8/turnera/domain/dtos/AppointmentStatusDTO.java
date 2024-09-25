ce933e636469642d6ba45e14570911108fb8f22d
Facundo Torres <ftorres@ubicuagroup.com>
2024-09-25 00:37:56 -0300

    agrego fecha de creado a estados de turnos

--- src/main/java/com/f8/turnera/domain/dtos/AppointmentStatusDTO.java
+++ src/main/java/com/f8/turnera/domain/dtos/AppointmentStatusDTO.java
@@ -1,5 +1,7 @@
 package com.f8.turnera.domain.dtos;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
@@ -8,5 +10,5 @@ public class AppointmentStatusDTO {
     private Long id;
     private AppointmentStatusEnum status;
     private String observations;
-
+    private LocalDateTime createdDate;
 }
